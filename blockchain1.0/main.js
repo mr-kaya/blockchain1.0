d83f6f36bc4feb5f3af415de1d65ea238fb48893
mustafa kaya <kaya.mustafa.467@gmail.com>
2020-11-18 07:38:15 +0300

    Implementing Proof-of-Work in Javascript

--- blockchain1.0/main.js
+++ blockchain1.0/main.js
@@ -8,16 +8,27 @@ class Block {
 		this.timestamp = timestamp;
 		this.previousHash = previousHash;
 		this.hash = this.calculateHash();
+		this.nonce = 0;
 	}
 
 	calculateHash() {
-		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+	}
+
+	mineBlock(difficulty) {
+		while (this.hash.substring(0, difficulty) !== Array(difficulty).join("0")) {
+			this.nonce++;
+			this.hash = this.calculateHash();
+		}
+
+		console.log("Block mined: " + this.hash);
 	}
 }
 
 class Blockchain {
 	constructor() {
 		this.chain = [this.createGenesisBlock()];
+		this.difficulty = 6; //mining bloklarýnýn, hash kodlarýnýn baþýndaki sýfýrlarýn ('0') kaç tane olacaðýný belirler.
 	}
 
 	createGenesisBlock() {
@@ -30,7 +41,7 @@ class Blockchain {
 
 	addBlock(newBlock) {
 		newBlock.previousHash = this.getLatestBlock().hash;
-		newBlock.hash = newBlock.calculateHash();
+		newBlock.mineBlock(this.difficulty);
 		this.chain.push(newBlock);
 	}
 
@@ -52,19 +63,9 @@ class Blockchain {
 }
 
 let blockchain = new Blockchain();
+
+console.log('mining block 1....');
 blockchain.addBlock(new Block(1, "10/07/2020", { amount: 4 }));
+
+console.log('mining block 2....');
 blockchain.addBlock(new Block(2, "12/07/2020", { amount: 10 }));
-
-console.log(JSON.stringify(blockchain, null, 4));
-
-console.log('Blockchain verileri dogru mu?' + blockchain.isChainValid()); //Veriler Deðiþtimi
-
-blockchain.chain[1].data = { amount: 100 }//Eklersen Veriler Deðiþti Olur
-
-console.log('Blockchain verileri dogru mu?' + blockchain.isChainValid()); //Veriler Deðiþtimi
-console.log('Hahaha' + blockchain.isChainValid());
-console.log('Hahaha' + blockchain.isChainValid());
-console.log('Hahaha' + blockchain.isChainValid());
-console.log('Hahaha' + blockchain.isChainValid());
-console.log('Hahaha' + blockchain.isChainValid());
-console.log('Hahaha' + blockchain.isChainValid());
